b473d123d3666b85c5fd3e342ee04a2982923301
tramo54 <tramo54@wgu.edu>
2025-08-29 13:08:34 -0400

    Some Styling and Commenting to easily understand the code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let animationId = null;
 
 const canvas = document.getElementById('rocketCanvas');
@@ -9,19 +11,22 @@ canvas.height = 600;
 ctx.fillStyle = "white";
 ctx.fillRect(190, 500, 20, 50);
 
-const g = 9.81; 
 
+const g = 9.81; 
 let rocketX = 190; 
 let rocketY = 500;
-
 let velocity = 0;
 let acceleration = 0;
 let time = 0;
 let running = false;
 let safetyFactor = 1.05;
 let burnTime = 0;
+
+// calling the drawRocket function so that the rocket can appear before launch 
 drawRocket();
 
+
+//click listener to be able to tell when the launch button is hit 
 
 document.getElementById("launchButton").addEventListener("click", () => {
 
@@ -32,6 +37,8 @@ document.getElementById("launchButton").addEventListener("click", () => {
     const dragCoefficient = parseFloat(document.getElementById("drag").value);
 
 
+
+    // minimum thurst alert to tell the user they need more thrust to move the rocket upward
     let minimumThrust = Math.round(mass * g * safetyFactor);
 
     if (thrust < minimumThrust){
@@ -102,6 +109,7 @@ function drawRocket() {
 
 
 
+    // burn time flame 
     if (time < burnTime) {
     let flameHeight = 10 + Math.random() * 5; 
     let flameGradient = ctx.createLinearGradient(rocketX + 10, rocketY + 50, rocketX + 10, rocketY + 50 + flameHeight);
@@ -118,6 +126,10 @@ function drawRocket() {
 }
 }
 
+
+
+
+// this function will update the rocket with the inputted peramters 
 function updateRocket( mass, thrust, burnTime, dragCoefficient){
     const dt = 0.1;
 
@@ -138,6 +150,7 @@ function updateRocket( mass, thrust, burnTime, dragCoefficient){
 
     rocketY -=  velocity * dt * 20; //scale factor for visibility 
 
+    // telemetry data
     document.getElementById("vel").textContent = `Velocity: ${velocity.toFixed(2)} m/s`;
     document.getElementById("acc").textContent = `Acceleration: ${acceleration.toFixed(2)} m/s²`;
     document.getElementById("fuel").textContent = `Fuel time left: ${Math.max(0, (burnTime - time)).toFixed(2)} s`;
